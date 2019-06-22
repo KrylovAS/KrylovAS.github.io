a967eb6b618d53ef07efce7e5a85e526ac640edc
KrylovAS <s1nii@mail.ru>
2019-06-22 18:11:15 +0300

    dfd

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
-'use strict';
+"use strict";
 function writeTextByJS(id, text, speed) {
-	let ele = document.getElementById(id),
-		txt = text.join('').split('');
-	console.log(txt);
-	let interval = setInterval(function() {
-		if (!txt[0]) {
-			return clearInterval(interval);
-		}
+    let ele = document.getElementById(id),
+        txt = text.join("").split("");
+    console.log(txt);
+    let interval = setInterval(
+        function() {
+            if (!txt[0]) {
+                return clearInterval(interval);
+            }
 
-		ele.innerHTML += txt.shift();
-	}, speed != undefined ? speed : 100);
+            ele.innerHTML += txt.shift();
+        },
+        speed != undefined ? speed : 100
+    );
 
-	return false;
+    return false;
 }
 
-writeTextByJS('demo', [ 'Воплотим в жизнь интерьер, который будет\n', 'радовать лаконичностью и уютом\n' ], 100);
+writeTextByJS(
+    "demo",
+    [
+        "Воплотим в жизнь интерьер, который будет\n",
+        "радовать лаконичностью и уютом\n"
+    ],
+    100
+);
